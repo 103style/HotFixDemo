3a385ae8b39ec21edccfba5522cbf8b0b64a7023
103style <wtimexiaoke@gmail.com>
2019-09-19 13:34:49 +0800

    init

---
+++ app/src/main/java/com/lxk/classloaderdemo/MainActivity.java
@@ -0,0 +1,18 @@
+package com.lxk.classloaderdemo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @author https://github.com/103style
+ * @date 2019/9/19 13:34
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
